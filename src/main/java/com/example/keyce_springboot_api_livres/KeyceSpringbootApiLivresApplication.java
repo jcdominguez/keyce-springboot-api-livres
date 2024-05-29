a79a613d7bbf35024c74578b589e18d258a12509
jcdominguez <jcd.pro@gmail.com>
2024-05-29 09:20:44 +0200

    configuration BD

---
+++ src/main/java/com/example/keyce_springboot_api_livres/KeyceSpringbootApiLivresApplication.java
@@ -0,0 +1,13 @@
+package com.example.keyce_springboot_api_livres;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeyceSpringbootApiLivresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KeyceSpringbootApiLivresApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/keyce_springboot_api_livres/KeyceSpringbootApiLivresApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.keyce_springboot_api_livres;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KeyceSpringbootApiLivresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
